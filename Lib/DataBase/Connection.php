b9665c246d963dcb10b17e864c8995e8319f5ddf
EDnotSheeran <juninho.tab3@gmail.com>
2020-01-30 16:05:14 -0300

    tratamento do erro ao conectar ao banco de dados

--- Lib/DataBase/Connection.php
+++ Lib/DataBase/Connection.php
@@ -1,14 +1,17 @@
 <?php
 
 abstract class Connection{
+ 
+        private static $conn;
 
-    private static $conn;
-
-    public static function getConn(){
-        if(self::$conn == null){
-            self::$conn = new PDO('mysql: host=localhost; dbname=Banco_Postagens;', 'root', '');
+        public static function getConn(){
+            try{
+                if(self::$conn == null){
+                    self::$conn = new PDO('mysql: host=localhost; dbname=Banco_Postagens;', 'root', '');
+                }
+                return self::$conn;
+            }catch(Exception $e){
+                throw new Exception('Falha ao conectar ao banco de dados');
+            }
         }
-        return self::$conn;
-    }
-
 }
